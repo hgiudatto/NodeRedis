77523208850943c5c72d7f641052a03294ecc1ed
Hector <hector.giudatto@gmail.com>
2022-11-06 08:50:41 -0300

    Completed Ok

--- bin/getkey.js
+++ bin/getkey.js
@@ -4,7 +4,11 @@ require("dotenv").config({ path: "./dev.env" });
 
 console.log("getkey");
 
+const key = process.argv[2];
+
 fs.readFile(process.env.FILE_PATH, "utf8", (err, data) => {
   err && console.log(`Error: ${err}`);
+  const myData = JSON.parse(data);
+  console.log(myData[key]);
   console.log(JSON.parse(data));
 });
--- bin/setkey.js
+++ bin/setkey.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 const fs = require("fs");
 require("dotenv").config({ path: "./dev.env" });
-
-console.log("setkey");
+const key = process.argv[2];
+const value = process.argv[3];
 
 fs.readFile(process.env.FILE_PATH, "utf8", (err, data) => {
   err && console.log(`Error: ${err}`);
   let myData = JSON.parse(data);
-  myData["test"] = "value";
+  myData[key] = value;
   fs.writeFile(process.env.FILE_PATH, JSON.stringify(myData), "utf8", (err) => {
     err && console.log(`Error: ${err}`);
+    console.log(`${key} is set to ${value}`);
   });
 });
